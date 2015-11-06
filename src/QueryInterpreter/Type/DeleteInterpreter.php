5cfbee10691b9f2a40ec2fdeb428539499a255fd
Danny Smart <dannysmart@silktide.com>
2015-11-06 17:20:41 +0000

    Create delete interpreter

---
+++ src/QueryInterpreter/Type/DeleteInterpreter.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Silktide\Reposition\Sql\QueryInterpreter\Type;
+use Silktide\Reposition\QueryBuilder\QueryToken\Entity;
+use Silktide\Reposition\QueryBuilder\TokenSequencerInterface;
+
+/**
+ * DeleteInterpreter
+ */
+class DeleteInterpreter extends AbstractSqlQueryTypeInterpreter
+{
+    public function supportedQueryType()
+    {
+        return TokenSequencerInterface::TYPE_DELETE;
+    }
+
+    public function interpretQuery(TokenSequencerInterface $query)
+    {
+        $this->query = $query;
+
+        $sql = "DELETE FROM " . $this->renderArbitraryReference($query->getEntityMetadata()->getCollection());
+
+        while($token = $query->getNextToken()) {
+            $sql .= " " . $this->renderToken($token);
+        }
+
+        return $sql;
+    }
+
+    protected function renderEntity(Entity $token)
+    {
+        return "";
+    }
+
+
+}
---
+++ test/QueryInterpreter/Type/DeleteInterpreterTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Silktide\Reposition\Sql\Test\QueryInterpreter\Type;
+
+use Silktide\Reposition\Sql\QueryInterpreter\Type\DeleteInterpreter;
+use Silktide\Reposition\QueryBuilder\TokenSequencerInterface;
+
+/**
+ * DeleteInterpreterTest
+ */
+class DeleteInterpreterTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $collection = "test_table";
+
+    /**
+     * @dataProvider sequenceProvider
+     *
+     * @param array $tokens
+     * @param $expectedSql
+     */
+    public function testInterpretation(array $tokens, $expectedSql)
+    {
+        /** @var TokenSequencerInterface $tokenSequencer */
+        $tokenSequencer = $this->createMockTokenSequencer($tokens);
+
+        $interpreter = new DeleteInterpreter();
+
+        $this->assertEquals($expectedSql, $interpreter->interpretQuery($tokenSequencer));
+    }
+
+    protected function createMockTokenSequencer(array $tokens)
+    {
+        $sequence = [];
+        foreach ($tokens as $tokenArgs) {
+            $token = \Mockery::mock("Silktide\\Reposition\\QueryBuilder\\QueryToken\\" . ucfirst($tokenArgs[0]));
+            if (isset($tokenArgs[1])) {
+                $token->shouldReceive("getType")->andReturn($tokenArgs[1]);
+            }
+            if (isset($tokenArgs[2])) {
+                $token->shouldReceive("getValue")->andReturn($tokenArgs[2]);
+            }
+            if (isset($tokenArgs[3])) {
+                $token->shouldReceive("getAlias")->andReturn($tokenArgs[3]);
+            }
+            $sequence[] = $token;
+        }
+        $sequence[] = false;
+
+        $metadata = \Mockery::mock("Silktide\\Reposition\\Metadata\\EntityMetadata");
+        $metadata->shouldReceive("getCollection")->andReturn($this->collection);
+
+        $tokenSequencer = \Mockery::mock("Silktide\\Reposition\\QueryBuilder\\TokenSequencerInterface");
+        $tokenSequencer->shouldReceive("getNextToken")->andReturnValues($sequence);
+        $tokenSequencer->shouldReceive("getEntityMetadata")->andReturn($metadata);
+
+        return $tokenSequencer;
+    }
+
+    public function sequenceProvider()
+    {
+        $defaultSql = "DELETE FROM `{$this->collection}`";
+
+        return [
+            [ // #0 simple full table delete
+                [],
+                $defaultSql
+            ],
+            [ // #1 simple delete by ID
+                [
+                    ["token", "where"],
+                    ["reference", "field", "id", ""],
+                    ["value", "operator", "="],
+                    ["value", "int", "value"]
+                ],
+                $defaultSql . " WHERE `id` = :int_0"
+            ],
+            [ // #2 limited delete in order
+                [
+                    ["token", "sort"],
+                    ["reference", "field", "timestamp", ""],
+                    ["value", "sort-direction", "desc"],
+                    ["reference", "field", "active", ""],
+                    ["token", "limit"],
+                    ["value", "int", 5]
+                ],
+                $defaultSql . " ORDER BY `timestamp` DESC, `active` LIMIT :int_0"
+            ]
+        ];
+    }
+
+}
